68b759acc307433afdd0f57731fce4119018e01d
12Perseus21 <collintv123@gmail.com>
2025-08-23 11:07:03 +0800

    LATEST UPDATE WITH ARCS

--- Globe.js
+++ Globe.js
@@ -3,12 +3,18 @@ import * as THREE from 'https://esm.sh/three';
 
 const arcs = [
   { name: "Crane", startLat: 48.776320, startLng: -108.360246, endLat: 33.392742, endLng: -102.032121, color: 'magenta', months: [8,9,10,3,4] },
-  { name: "Eagle", startLat: 45, startLng: -100, endLat: 25, endLng: -70, color: 'cyan', months: [9,10,11,2,3] },
-  { name: "Goose", startLat: 50, startLng: -90, endLat: 30, endLng: -60, color: 'lime', months: [10,11,0,1,2] },
-  { name: "Warbler", startLat: 35, startLng: -80, endLat: 15, endLng: -50, color: 'yellow', months: [2,3,4,5] },
-  { name: "Stork", startLat: 10, startLng: -50, endLat: 30, endLng: -20, color: 'pink', months: [3,4,5,6] },
-  { name: "Swallow", startLat: 29.487442192568476, startLng: -96.77888488946694, endLat: -29.905878792375937, endLng: -63.776867262107864, color: 'blue', months: [4,5,6,7] },
-  { name: "Hawk", startLat: 10, startLng: -60, endLat: 30, endLng: -30, color: 'orange', months: [7,8,9,10] }
+  { name: "Crane", startLat: 51.977490, startLng: -93.419713, endLat: 34.859608, endLng: -112.666747, color: 'magenta', months: [8,9,10,3,4] },
+  { name: "Eagle", startLat: 48.803961, startLng: 9.629311, endLat: 53.959159, endLng: 22.250249, color: 'cyan', months: [9,10,11,2,3] },
+  { name: "Eagle", startLat: 25.762313, startLng: 83.175663, endLat: 28.509404, endLng: 96.802460, color: 'cyan', months: [9,10,11,2,3] },
+  { name: "Goose", startLat: 53.654084, startLng: -7.070723, endLat: 64.570282, endLng: -17.823010, color: 'lime', months: [10,11,0,1,2] },
+  { name: "Goose", startLat: 64.570282, startLng: -17.823010, endLat: 67.437727, endLng: -45.275332, color: 'lime', months: [10,11,0,1,2] },
+  { name: "Warbler", startLat: 49.972591, startLng: -90.373420, endLat: 37.650520, endLng: -95.912134, color: 'yellow', months: [2,3,4,5] },
+  { name: "Warbler", startLat: 48.158397, startLng: -107.266500, endLat: 28.380562, endLng: -97.850685, color: 'yellow', months: [2,3,4,5] },
+  { name: "Stork", startLat: 50.857489, startLng: 6.688258, endLat: 45.447740, endLng: 21.348121, color: 'pink', months: [3,4,5,6] },
+  { name: "Stork", startLat: 24.011547, startLng: 102.653979, endLat: 32.824697, endLng: 113.592493, color: 'pink', months: [3,4,5,6] },
+  { name: "Swallow", startLat: 28.433086, startLng: 101.501193, endLat: 16.143856, endLng: 120.808176, color: 'blue', months: [4,5,6,7] },
+  { name: "Swallow", startLat: 16.143856, startLng: 120.808176, endLat: 10.260747, endLng: 124.051068, color: 'blue', months: [4,5,6,7] },
+  { name: "Hawk", startLat: 41.982003, startLng: -117.382852, endLat: 44.745258, endLng: -94.643698, color: 'orange', months: [7,8,9,10] }
 ];
 
 const globe = Globe({ animateIn: false })(document.getElementById('globeViz'))
